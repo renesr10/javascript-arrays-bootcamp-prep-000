3060a3d0dfcb2f6de7a3256741bc913a15040bb5
Rene Santana <renesantana10@gmail.com>
2018-07-18 22:36:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,6 +29,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0, array.length)
+  array.slice(array.length)
   return array
 }
